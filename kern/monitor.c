44ae7f17c4d9967d6b1ce3a0238898702c8654e2
Zhifan Zhu <zhifanzhu@yeah.net>
2018-10-06 19:36:17 +0800

    finish lab2 challenge

--- kern/monitor.c
+++ kern/monitor.c
@@ -11,7 +11,6 @@
 #include <kern/monitor.h>
 #include <kern/kdebug.h>
 #include <kern/pmap.h>
-#include <kern/showmappings.h>
 
 #define CMDBUF_SIZE	80	// enough for one VGA text line
 
@@ -27,7 +26,7 @@ static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
 	{ "backtrace", "Display backtrace information", mon_backtrace},
-	{ "pg", "Display physical page mappings", mon_showmappings },
+	{ "pg", "Display/Setting physical page mappings", mon_showmappings },
 };
 
 /***** Implementations of basic kernel monitor commands *****/
--- kern/showmappings.h
+++
@@ -1,9 +0,0 @@
-#ifndef JOS_KERN_SHOWMAPPINGS_H
-#define JOS_KERN_SHOWMAPPINGS_H
-
-
-struct Trapframe;
-
-int mon_showmappings(int argc, char **argv, struct Trapframe *tf);
-
-#endif	// !JOS_KERN_SHOWMAPPINGS_H
