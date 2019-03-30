3ce5305f64a6685f0be4749b47f691abf810c6e0
Zhifan Zhu <zhifanzhu@yeah.net>
2019-03-30 16:11:57 +0800

    Ex 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,11 +1,19 @@
 #include <kern/e1000.h>
+#include <inc/types.h>
+
+#include <kern/pci.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
-#include <kern/pci.h>
+
+volatile uint32_t *e1000_bar0;
 
 int 
-pci_82540em_attach(struct pci_func *f)
+pci_e1000_attach(struct pci_func *f)
 {
     pci_func_enable(f);
+    uint32_t bar0_pa = f->reg_base[0], bar0_size = f->reg_size[0];
+    e1000_bar0 = mmio_map_region(bar0_pa, bar0_size);
+    assert(e1000_bar0[E1000_STATUS] == 0x80080784); // full duplex link 1000 MB/s
     return 1;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,9 +2,66 @@
 #define JOS_KERN_E1000_H
 
 #define PCI_VENDOR_DEFAULT          0x8086
-#define PCI_DEVICE_82540EM_A        0x100e
+#define PCI_DEVICE_E1000            0x100e
+
+/* Copy from e1000_hw.h
+ * Structures, enums, and macros for the MAC
+ */
+
+/* PCI Device IDs */
+#define E1000_DEV_ID_82540EM             0x100E
+
+/* Register Set. (82543, 82544)
+ *
+ * Registers are defined to be 32 bits and  should be accessed as 32 bit values.
+ * These registers are physically located on the NIC, but are mapped into the
+ * host memory address space.
+ *
+ * RW - register is both readable and writable
+ * RO - register is read only
+ * WO - register is write only
+ * R/clr - register is read only and is cleared when read
+ * A - register array
+ */
+#define E1000_CTRL     0x00000/4  /* Device Control - RW */
+#define E1000_CTRL_DUP 0x00004/4  /* Device Control Duplicate (Shadow) - RW */
+#define E1000_STATUS   0x00008/4  /* Device Status - RO */
+
+/* Device Status */
+#define E1000_STATUS_FD         0x00000001      /* Full duplex.0=half,1=full */
+#define E1000_STATUS_LU         0x00000002      /* Link up.0=no,1=link */
+#define E1000_STATUS_FUNC_MASK  0x0000000C      /* PCI Function Mask */
+#define E1000_STATUS_FUNC_SHIFT 2
+#define E1000_STATUS_FUNC_0     0x00000000      /* Function 0 */
+#define E1000_STATUS_FUNC_1     0x00000004      /* Function 1 */
+#define E1000_STATUS_TXOFF      0x00000010      /* transmission paused */
+#define E1000_STATUS_TBIMODE    0x00000020      /* TBI mode */
+#define E1000_STATUS_SPEED_MASK 0x000000C0
+#define E1000_STATUS_SPEED_10   0x00000000      /* Speed 10Mb/s */
+#define E1000_STATUS_SPEED_100  0x00000040      /* Speed 100Mb/s */
+#define E1000_STATUS_SPEED_1000 0x00000080      /* Speed 1000Mb/s */
+#define E1000_STATUS_LAN_INIT_DONE 0x00000200   /* Lan Init Completion
+                                                   by EEPROM/Flash */
+#define E1000_STATUS_ASDV       0x00000300      /* Auto speed detect value */
+#define E1000_STATUS_DOCK_CI    0x00000800      /* Change in Dock/Undock state. Clear on write '0'. */
+#define E1000_STATUS_GIO_MASTER_ENABLE 0x00080000 /* Status of Master requests. */
+#define E1000_STATUS_MTXCKOK    0x00000400      /* MTX clock running OK */
+#define E1000_STATUS_PCI66      0x00000800      /* In 66Mhz slot */
+#define E1000_STATUS_BUS64      0x00001000      /* In 64 bit slot */
+#define E1000_STATUS_PCIX_MODE  0x00002000      /* PCI-X mode */
+#define E1000_STATUS_PCIX_SPEED 0x0000C000      /* PCI-X bus speed */
+#define E1000_STATUS_BMC_SKU_0  0x00100000 /* BMC USB redirect disabled */
+#define E1000_STATUS_BMC_SKU_1  0x00200000 /* BMC SRAM disabled */
+#define E1000_STATUS_BMC_SKU_2  0x00400000 /* BMC SDRAM disabled */
+#define E1000_STATUS_BMC_CRYPTO 0x00800000 /* BMC crypto disabled */
+#define E1000_STATUS_BMC_LITE   0x01000000 /* BMC external code execution disabled */
+#define E1000_STATUS_RGMII_ENABLE 0x02000000 /* RGMII disabled */
+#define E1000_STATUS_FUSE_8       0x04000000
+#define E1000_STATUS_FUSE_9       0x08000000
+#define E1000_STATUS_SERDES0_DIS  0x10000000 /* SERDES disabled on port 0 */
+#define E1000_STATUS_SERDES1_DIS  0x20000000 /* SERDES disabled on port 1 */
 
 struct pci_func;
-int pci_82540em_attach(struct pci_func *);
+int pci_e1000_attach(struct pci_func *);
 
 #endif	// JOS_KERN_E1000_H
